329e141289053e304cf9b638d82c18e5e973f652
Grzegorz Adamczyk <46348032+slashbinbash@users.noreply.github.com>
2020-07-27 11:14:43 +0200

    First test case. Try convert "I" to 1

--- TestClass.cs
+++ TestClass.cs
@@ -1,5 +1,6 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -7,14 +8,17 @@ using NUnit.Framework;
 namespace RomanNumeralsKata
 {
     [TestFixture]
-    public class TestClass
+    public class RomanNumeralsTests
     {
         [Test]
-        public void TestMethod()
+        public void RomanNumeralsConverterTest()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            Assert.AreEqual(1,RomanNumeralsConverter("I"));
+        }
+
+        private double RomanNumeralsConverter(string romanNumeral)
+        {
+            return 1;
         }
     }
 }
